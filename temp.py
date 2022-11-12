a7108684c1bf7ac3efe888ae0419179d2ee3a67c
Maksim Arkhipov <manjanfe@gmail.com>
2022-11-12 08:27:38 +0700

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, Union, Optional
-
-def we_crash_all(name: Union[str, int, bool, None]) -> str:    
-    return f'Привет, {str(name)}, мы всё сломали!'
-
-
-print(we_crash_all('Наташа'))
-
